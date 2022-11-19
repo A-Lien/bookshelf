d4ce3c410e1d81b5b5ce1739478a6673ab305085
Ashton Lien <s-hlien@hotmail.com>
2022-11-19 09:36:54 -0800

    main.c, bookshelf.c/.h created and initialized

---
+++ main.c
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////
+//Author: Ashton Lien
+//Start Date: 11/19/22
+//Last Updated: 11/19/22
+//File Name: main.c
+//Description: Propgrams display, organizes, and catagorizes books based on
+//             information given to it by the user
+////////////////////////////////////////////////////////////////////////////
+
+#include "bookshelf.c"
+
